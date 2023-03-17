adb8c883ae074a0828fbab75e574a14cf2294ca3
timholx <timholx@gmail.com>
2023-03-18 01:17:30 +0800

    Update Trader.py

--- round0.0/Trader.py
+++ round0.0/Trader.py
@@ -29,42 +29,55 @@ class Trader:
             self.pos[product] += pos_delta
 
         # Iterate over all the keys (the available products) contained in the order depths
-        for product in state.order_depths.keys():
-            # Initialise the list of Orders to be sent as an empty list
-            orders: list[Order] = []
+        product = "PEARLS"
+        # Initialise the list of Orders to be sent as an empty list
+        orders: list[Order] = []
 
-            # Obtain OrderDepth object for the product
-            order_depth: OrderDepth = state.order_depths[product]
-            # Sort the sell orders and buy orders
-            best_ask = min(order_depth.sell_orders.keys()) if len(
-                order_depth.sell_orders) != 0 else None
-            best_ask_volume = order_depth.sell_orders[best_ask] if best_ask is not None else None
-            best_bid = max(order_depth.buy_orders.keys()) if len(
-                order_depth.buy_orders) != 0 else None
-            best_bid_volume = order_depth.buy_orders[best_bid] if best_bid is not None else None
-            avg = (best_bid + best_ask) / \
-                2 if best_bid is not None and best_ask is not None else None
+        # Obtain OrderDepth object for the product
+        order_depth: OrderDepth = state.order_depths[product]
+        # Sort the sell orders and buy orders
+        best_ask = min(order_depth.sell_orders.keys()) if len(
+            order_depth.sell_orders) != 0 else None
+        best_ask_volume = order_depth.sell_orders[best_ask] if best_ask is not None else None
+        best_bid = max(order_depth.buy_orders.keys()) if len(
+            order_depth.buy_orders) != 0 else None
+        best_bid_volume = order_depth.buy_orders[best_bid] if best_bid is not None else None
+        avg = (best_bid + best_ask) / \
+            2 if best_bid is not None and best_ask is not None else None
 
-            if avg is not None:
-                self.sma[product].append(avg)
-                acceptable_price = np.array(self.sma[product]).mean() if len(
-                    self.sma[product]) != 0 else None
+        if avg is not None:
+            self.sma[product].append(avg)
+            if len(self.sma[product]) != 0:
+                if len(self.sma[product]) < 7:
+                    acceptable_price = np.array(self.sma[product]).mean()
+                else:
+                    acceptable_price = np.array(self.sma[product])[-7:].mean()
+        else:
+            acceptable_price = 4950
 
-            if acceptable_price is not None and best_ask < acceptable_price:
+        if acceptable_price is not None and best_ask < acceptable_price:
+            if best_ask_volume is not None:
                 buyable_volume = min(-best_ask_volume,
-                                     self.pos_limit[product] - self.pos[product])
-                print("BUY", str(buyable_volume) + "x", best_ask)
-                orders.append(
-                    Order(product, best_ask, buyable_volume))
+                                 self.pos_limit[product] - self.pos[product])
+            else:
+                buyable_volume = self.pos_limit[product] - self.pos[product]
+            
+            if buyable_volume > 0:
+                print("BUY", product, str(buyable_volume) + "x", best_ask)
+                orders.append(Order(product, best_ask, buyable_volume))
 
-            if acceptable_price is not None and best_bid > acceptable_price:
+        if acceptable_price is not None and best_bid > acceptable_price:
+            if best_bid_volume is not None:
                 sellable_volume = max(-best_bid_volume, -
-                                      self.pos_limit[product] - self.pos[product])
-                print("SELL", str(sellable_volume) + "x", best_bid)
-                orders.append(
-                    Order(product, best_bid, sellable_volume))
+                                  self.pos_limit[product] - self.pos[product])
+            else:
+                sellable_volume = -self.pos_limit[product] - self.pos[product]
+            
+            if sellable_volume < 0:
+                print("SELL", product, str(sellable_volume) + "x", best_bid)
+                orders.append(Order(product, best_bid, sellable_volume))
 
-            # Add all the above orders to the result dict
-            result[product] = orders
+        # Add all the above orders to the result dict
+        result[product] = orders
 
         return result
