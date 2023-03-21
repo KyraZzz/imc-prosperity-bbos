d19d8ff8083d070a012e7356dc8394e4af71f046
timholx <timholx@gmail.com>
2023-03-21 22:53:37 +0800

    Update manual_round1.cpp

--- manual_round1.cpp
+++ manual_round1.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     float exch[4][4] = {{1,0.5,1.45,0.75},{1.95,1,3.1,1.49},{0.67,0.31,1,0.48},{1.34,0.64,1.98,1}};
 
-    /// Enumerate all possibilities from (0,0,0,0,0) to (3,3,3,3,3)
+    /// Enumerate all possibilities from (0,0,0,0) to (3,3,3,3)
     float maxval = -1;
     for(int n = 0; n < 256; n++){
         int arr[6];
