17fc0f35c5c4da19d877e8871ef139157206f155
timholx <timholx@gmail.com>
2023-03-21 22:53:19 +0800

    Update manual_round1.cpp

--- manual_round1.cpp
+++ manual_round1.cpp
@@ -5,12 +5,12 @@ int main(){
 
     /// Enumerate all possibilities from (0,0,0,0,0) to (3,3,3,3,3)
     float maxval = -1;
-    for(int n = 0; n < 1024; n++){
+    for(int n = 0; n < 256; n++){
         int arr[6];
-        arr[0] = 3;
+        arr[0] = arr[5] = 3;
         int k = n;
-        for(int i = 0; i < 5; i++){
-            arr[5-i] = (n % 4);
+        for(int i = 0; i < 4; i++){
+            arr[4-i] = (n % 4);
             n /= 4;
         }
         n = k;
@@ -25,7 +25,7 @@ int main(){
         maxval = max(maxval, val);
         if(maxval == val){
             cout << n << ": ";
-            for(int j = 1; j < 6; j++){
+            for(int j = 0; j < 6; j++){
                 cout << arr[j] << " ";
             }
             cout << ": " << val << "\n";
@@ -33,4 +33,3 @@ int main(){
     }
     return 0;
 }
-
